e520769f325a1c73f12530e09609ed5a8debf436
Matthew Pomes <the10thwiz@gmail.com>
2021-10-15 09:59:45 -0500

    Add clean function

--- src/main.rs
+++ src/main.rs
@@ -5,65 +5,96 @@
 //! TODO:
 //! - Support custom recipes (A few more options need to be added...)
 //! - More builtin options
-//! - Clean operation
+//! + Clean operation
+//! - Log files allowing clean to avoid running all files, and potentially faster opteration?
 
-use std::{borrow::Cow, collections::{HashMap, HashSet}, fs::File, io::{Error, Read, Write}, path::PathBuf, process::{Command, Output, Stdio}, str::FromStr};
+use std::{
+    borrow::Cow,
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::{Error, Read, Write},
+    path::{Path, PathBuf},
+    process::{Command, Output, Stdio},
+    str::FromStr,
+};
 
-use structopt::StructOpt;
+//use structopt::{clap::Shell, StructOpt};
+use clap::{Clap, IntoApp};
+use clap_generate::{
+    generators::{Bash, Elvish, Fish, PowerShell, Zsh},
+    Shell,
+};
 
 /// Command line tool to automatically build latex documents
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Clap)]
 struct Options {
     /// Compile to dvi rather than pdf
-    #[structopt(short, long)]
+    #[clap(short, long)]
     dvi: bool,
     /// Sets output file for itermediate files (TODO)
-    #[structopt(short, long, default_value = "./")]
+    #[clap(short, long, default_value = "./")]
     output_dir: String,
-    /// Automatically clean up generated files (TODO)
-    #[structopt(short, long)]
+    /// Automatically clean up generated files
+    ///
+    /// Note that this still runs the full build process, since latexmk doesn't keep a log of the
+    /// generated files between runs
+    #[clap(short, long)]
     clean: bool,
     /// Files to compile [default: ./*.tex]
     files: Vec<PathBuf>,
+    /// Output shell completion script
+    ///
+    /// Supported shells: [Bash, Zsh]
+    /// Note that this overrides any other settings specified
+    #[clap(long)]
+    shell_completion: Option<Shell>,
 }
-
-// Rules:
-// - .tex -> .pdf via pdflatex
-// - .sagetex.sage -> .sagetex.sout via sage
 
 fn make_cmds() -> HashMap<String, Recipe> {
     let mut map = HashMap::new();
+    // pdflatex
     map.insert(
         "pdf".into(),
         Recipe {
             uses: "tex",
             extras: &[],
+            generated: &["fls", "synctex.gz"],
+            generated_dirs: &[],
             script:
                 "pdflatex -recorder -file-line-error -interaction nonstopmode -synctex 1 \"%I\""
                     .into(),
         },
     );
+    // dvilualatex
     map.insert(
         "dvi".into(),
         Recipe {
             uses: "tex",
             extras: &[],
+            generated: &[],
+            generated_dirs: &[],
             script: "dvilualatex --recorder --file-line-error --interaction=nonstopmode --synctex=1 \"%I\"".into(),
         },
     );
+    // sage
     map.insert(
         "sagetex.sout".into(),
         Recipe {
             uses: "sagetex.sage",
             extras: &[],
+            generated: &["sagetex.sage.py", "sagetex.scmd"],
+            generated_dirs: &["sage-plots-for-"],
             script: "sage \"%I\"".into(),
         },
     );
+    // bibtex
     map.insert(
         "bbl".into(),
         Recipe {
-            uses: "tex",
+            uses: "aux",
             extras: &["bib"],
+            generated: &["blg"],
+            generated_dirs: &[],
             script: "bibtex \"%N\"".into(),
         },
     );
@@ -88,13 +119,29 @@ impl Deps {
 struct Recipe {
     uses: &'static str,
     extras: &'static [&'static str],
+    generated: &'static [&'static str],
+    generated_dirs: &'static [&'static str],
     script: Cow<'static, str>,
+}
+
+fn with_parent<W>(path: &Path, f: impl FnOnce(&Path) -> W) -> W {
+    if let Some(p) = path.parent() {
+        if p.exists() {
+            f(p)
+        } else {
+            f(&PathBuf::from_str(".").unwrap())
+        }
+    } else {
+        f(&PathBuf::from_str(".").unwrap())
+    }
 }
 
 impl Recipe {
     fn check_file_times(&self, input_name: &PathBuf, output_name: &str) -> std::io::Result<bool> {
         // Check file times and only rebuild if needed
-        let output_time = File::open(input_name.with_file_name(output_name))?.metadata()?.modified()?;
+        let output_time = File::open(input_name.with_file_name(output_name))?
+            .metadata()?
+            .modified()?;
         let input_time = File::open(input_name)?.metadata()?.modified()?;
         for path in PathBuf::from_str(".").unwrap().read_dir()? {
             let path = path?;
@@ -111,34 +158,65 @@ impl Recipe {
         Ok(output_time > input_time)
     }
 
-    fn on_file(&self, path: &PathBuf, ext: &str) -> std::io::Result<Output> {
+    fn on_file(
+        &self,
+        path: &PathBuf,
+        ext: &str,
+        output: &mut HashSet<PathBuf>,
+    ) -> std::io::Result<Output> {
+        if let Ok(dir) = with_parent(path, |f| f.read_dir()) {
+            for file in dir {
+                if let Ok(file) = file {
+                    if file.file_type().map_or(false, |f| f.is_dir()) {
+                        let name = file.file_name();
+                        let name = name.to_str().unwrap_or("");
+                        if self.generated_dirs.iter().any(|gen| name.starts_with(gen)) {
+                            output.insert(file.path());
+                        }
+                    } else {
+                        let name = file.file_name();
+                        let name = name.to_str().unwrap_or("");
+                        if self.generated.iter().any(|gen| name.ends_with(gen)) {
+                            output.insert(file.path());
+                        }
+                    }
+                }
+            }
+        }
         let output_name = path.file_name().map_or("", |o| o.to_str().unwrap_or(""));
-        let input_name = format!("{}.{}", &output_name[..output_name.len() - ext.len() - 1], self.uses);
+        let input_name = format!(
+            "{}.{}",
+            &output_name[..output_name.len() - ext.len() - 1],
+            self.uses
+        );
+        println!("Running rule on {}", input_name);
 
-        if self.check_file_times(&path, &output_name)? {
+        // Note that this function will fail with an error if the file doesn't exist, but there
+        // is not harm is rebuilding the file if we don't need to.
+        if matches!(self.check_file_times(&path, &output_name), Ok(true))
+            || !path.with_file_name(&input_name).exists()
+        {
             return Command::new("true").output();
         }
 
         let mut cmd = Command::new("bash");
-        cmd.arg("-c")
-            .arg(
-                self.script
-                    .replace("%O", output_name)
-                    .replace("%I", &input_name)
-                    .replace("%N", &output_name[..output_name.len() - ext.len() - 1])
-                    .replace("%%", "%"),
-            );
+        cmd.arg("-c").arg(
+            self.script
+                .replace("%O", output_name)
+                .replace("%I", &input_name)
+                .replace("%N", &output_name[..output_name.len() - ext.len() - 1])
+                .replace("%%", "%"),
+        );
         if let Some(parent) = path.parent() {
             if let Ok(dir) = parent.canonicalize() {
                 cmd.current_dir(dir);
             }
         }
-        cmd.stdout(Stdio::piped())
-            .output()
+        cmd.stdout(Stdio::piped()).output()
     }
 
     fn run_for(&self, path: &PathBuf, ext: &str, deps: &mut Deps) -> std::io::Result<()> {
-        let output = self.on_file(path, ext)?;
+        let output = self.on_file(path, ext, &mut deps.output)?;
         let stdout = String::from_utf8_lossy(&output.stdout);
         deps.missing = find(&stdout);
         if !output.status.success() {
@@ -158,7 +236,6 @@ fn find(s: &str) -> HashSet<String> {
     while cur.len() > 0 {
         if let Some((_pre, rest)) = cur.split_once("No file ") {
             let filename = rest.split_once('\n').map_or(rest, |(r, _)| r);
-            println!("Missing: {}", filename);
             ret.insert(filename[..filename.len() - 1].into());
             cur = &rest[1..];
         } else {
@@ -169,8 +246,49 @@ fn find(s: &str) -> HashSet<String> {
 }
 
 fn main() -> std::io::Result<()> {
-    let mut options = Options::from_args();
-    eprintln!("{:?}", options);
+    let mut options = Options::parse();
+    if let Some(shell) = options.shell_completion {
+        match shell {
+            Shell::Bash => clap_generate::generate::<Bash, _>(
+                &mut Options::into_app(),
+                std::env::current_exe()?
+                    .file_name()
+                    .map_or("latexmk", |f| f.to_str().unwrap_or("latexmk")),
+                &mut std::io::stdout(),
+            ),
+            Shell::Elvish => clap_generate::generate::<Elvish, _>(
+                &mut Options::into_app(),
+                std::env::current_exe()?
+                    .file_name()
+                    .map_or("latexmk", |f| f.to_str().unwrap_or("latexmk")),
+                &mut std::io::stdout(),
+            ),
+            Shell::Fish => clap_generate::generate::<Fish, _>(
+                &mut Options::into_app(),
+                std::env::current_exe()?
+                    .file_name()
+                    .map_or("latexmk", |f| f.to_str().unwrap_or("latexmk")),
+                &mut std::io::stdout(),
+            ),
+            Shell::PowerShell => clap_generate::generate::<PowerShell, _>(
+                &mut Options::into_app(),
+                std::env::current_exe()?
+                    .file_name()
+                    .map_or("latexmk", |f| f.to_str().unwrap_or("latexmk")),
+                &mut std::io::stdout(),
+            ),
+            Shell::Zsh => clap_generate::generate::<Zsh, _>(
+                &mut Options::into_app(),
+                std::env::current_exe()?
+                    .file_name()
+                    .map_or("latexmk", |f| f.to_str().unwrap_or("latexmk")),
+                &mut std::io::stdout(),
+            ),
+            _ => todo!(),
+        }
+        return Ok(());
+    }
+    //eprintln!("{:?}", options);
     let base = if options.dvi { "dvi" } else { "pdf" };
 
     // Insert all files that end with .tex in the current directory if no files were specified
@@ -188,26 +306,23 @@ fn main() -> std::io::Result<()> {
     let mut deps = Deps::default();
 
     for file in options.files {
-        // TODO: select command & args by Options
         let _ = recipes.get(base).unwrap().run_for(&file, base, &mut deps);
-        let name = file.file_name().unwrap().to_str().expect("Unsupported filename");
+        let name = file
+            .file_name()
+            .unwrap()
+            .to_str()
+            .expect("Unsupported filename");
         collect_files(&name[..name.len() - ".tex".len()], &mut deps)?;
-        //println!("{:?}", deps);
 
         let mut rerun = false;
 
-        //let inital_time = File::open(&file)?.metadata()?.modified()?;
         for dep in deps.input.iter() {
-            //let dep_time = File::open(dep)?.metadata()?.modified()?;
-            //if dep_time > inital_time {
-                if build(dep)? {
-                    rerun = true;
-                }
-            //}
+            if build(dep, &mut deps.output)? {
+                rerun = true;
+            }
         }
         for dep in deps.missing.iter() {
-            println!("Missing {}", dep);
-            if build(&PathBuf::default().with_file_name(&dep))? {
+            if build(&PathBuf::default().with_file_name(&dep), &mut deps.output)? {
                 rerun = true;
             }
         }
@@ -217,6 +332,20 @@ fn main() -> std::io::Result<()> {
             recipes.get(base).unwrap().run_for(&file, base, &mut deps)?;
         }
         deps.clean();
+    }
+    if options.clean {
+        println!("Cleaning up files");
+        for file in deps.output {
+            let name = file.file_name().map_or("", |s| s.to_str().unwrap_or(""));
+            // Protect pdf & dvi files
+            if !name.ends_with("pdf") && !name.ends_with("dvi") {
+                if let Err(_) = std::fs::remove_file(&file) {
+                    if let Err(_) = std::fs::remove_dir_all(&file) {
+                        println!("Couldn't remove {}", file.display());
+                    }
+                }
+            }
+        }
     }
     Ok(())
 }
@@ -254,13 +383,14 @@ fn collect_files(name: &str, deps: &mut Deps) -> std::io::Result<()> {
     Ok(())
 }
 
-fn build(dep: &PathBuf) -> std::io::Result<bool> {
+fn build(dep: &PathBuf, output: &mut HashSet<PathBuf>) -> std::io::Result<bool> {
     let name = dep.file_name().map_or("", |o| o.to_str().unwrap_or(""));
-    println!("Building {}", name);
+    //println!("Building {}", name);
     let recipes = make_cmds();
     for (makes, recipe) in recipes.iter() {
         if name.ends_with(makes) {
-            let output = recipe.on_file(dep, makes)?;
+            output.insert(dep.clone());
+            let output = recipe.on_file(dep, makes, output)?;
             if output.status.success() {
                 println!("Built {}", name);
                 return Ok(true);
